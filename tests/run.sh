4e482120cdd07d2da10bda7b6c5ff4a7afe198a7
Willie Northway <willn@umich.edu>
2017-12-22 01:04:44 -0400

    Add a php lint check to the phpunit testing

--- tests/run.sh
+++ tests/run.sh
@@ -1,4 +1,9 @@
 for i in `ls *Test.php`; do
+	# do a lint check first...
+	lint=`php -l $i | grep 'Errors'`;
+	[[ ! -z $lint ]] && echo "FAIL $i --> php lint issue" && continue
+
+	# now run phpunit
 	phpunit $i $1 2>&1 > $1.out;
 	if [[ $? -eq 0 ]]; then
 		echo "PASS $i"
