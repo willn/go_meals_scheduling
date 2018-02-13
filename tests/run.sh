1979dda86d567475865d3a11b7b0bcd8bae182df
Willie Northway <willn@umich.edu>
2018-02-12 23:42:28 -0500

    Check phplint first, and fail if it didn't pass cleanly

--- tests/run.sh
+++ tests/run.sh
@@ -1,9 +1,17 @@
-for i in `ls *Test.php`; do
-	# do a lint check first...
+# do a lint check first...
+errs=0
+for i in `find ../ -name "*.php"`; do
 	lint=`php -l $i | grep 'Errors'`;
-	[[ ! -z $lint ]] && echo "FAIL $i --> php lint issue" && continue
+	[[ ! -z $lint ]] && echo "FAIL $i --> php lint issue" && let errs=errs+1
+done
+if [ "$errs" -gt 0 ]
+then
+	exit
+fi
+echo "all files pass lint"
 
-	# now run phpunit
+# now run phpunit
+for i in `ls *Test.php`; do
 	phpunit $i $1 2>&1 > $1.out;
 	if [[ $? -eq 0 ]]; then
 		echo "PASS $i"
