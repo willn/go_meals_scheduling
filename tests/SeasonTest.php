efb2abeb625f185ed4337761a7b76219b2ea77d9
Willie Northway <willn@umich.edu>
2018-09-18 23:27:51 -0400

    Add unit tests to examine the structure of the season preferences arrays.

--- tests/SeasonTest.php
+++ tests/SeasonTest.php
@@ -5,6 +5,58 @@ require_once '../public/season.php';
  * This is simple example to ensure the testing framework functions properly.
  */
 class SeasonTest extends PHPUnit_Framework_TestCase {
+	public function test_get_num_shift_overrides() {
+		$overrides = get_num_shift_overrides();
+		$this->assertInternalType("array", $overrides);
+
+		if (!empty($overrides)) {
+			foreach($overrides as $name => $assignments) {
+				$this->assertInternalType("string", $name);
+				$this->assertInternalType("array", $assignments);
+				$this->assertNotEmpty($assignments);
+
+				foreach($assignments as $job_id => $num_assign) {
+					$this->assertInternalType("int", $job_id);
+					$this->assertInternalType("int", $num_assign);
+				}
+			}
+		}
+	}
+
+	public function test_get_skip_dates() {
+		$skips = get_skip_dates();
+		$this->assertInternalType("array", $skips);
+
+		if (!empty($skips)) {
+			foreach($skips as $month => $list_of_days) {
+				$this->assertInternalType("int", $month);
+				$this->assertInternalType("array", $list_of_days);
+				$this->assertNotEmpty($list_of_days);
+
+				foreach($list_of_days as $day) {
+					$this->assertInternalType("int", $day);
+				}
+			}
+		}
+	}
+
+	public function test_get_regular_day_overrides() {
+		$reg_overrides = get_regular_day_overrides();
+		$this->assertInternalType("array", $reg_overrides);
+
+		if (!empty($reg_overrides)) {
+			foreach($reg_overrides as $month => $list_of_days) {
+				$this->assertInternalType("int", $month);
+				$this->assertInternalType("array", $list_of_days);
+				$this->assertNotEmpty($list_of_days);
+
+				foreach($list_of_days as $day) {
+					$this->assertInternalType("int", $day);
+				}
+			}
+		}
+	}
+
 	/**
 	 * @dataProvider provide_get_current_season_months
 	 */
