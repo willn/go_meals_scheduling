4710a4d86cbb89fa215e5a0fa5e36b64b5d661eb
Willie Northway <willn@umich.edu>
2019-01-17 04:04:12 +0000

    Add unit tests for renderWorkerComments

--- tests/CalendarTest.php
+++ tests/CalendarTest.php
@@ -548,6 +548,82 @@ EOHTML;
 	}
 
 	/**
+	 * @dataProvider provideRenderWorkerComments
+	 */
+	public function testRenderWorkerComments($input, $expected) {
+		$result = $this->calendar->renderWorkerComments($input);
+		$result = remove_html_whitespace($result);
+		$expected = remove_html_whitespace($expected);
+		$this->assertEquals($expected, $result, print_r($debug, TRUE));
+	}
+
+	public function provideRenderWorkerComments() {
+		$basic_response = <<<EOHTML
+<h2 id="worker_comments">Comments</h2>
+<h2 id="confirm_checks">Confirm results check</h2>
+<div class="confirm_results"></div>
+EOHTML;
+
+		$small_data_file = file_get_contents('data/worker_comments.json');
+		$small_data = json_decode($small_data_file, TRUE);
+
+		$small_output = <<<EOHTML
+<h2 id="worker_comments">Comments</h2>
+		<fieldset>
+			<legend>apples - 2018-12-22 21:39:00</legend>
+
+		</fieldset>		<fieldset>
+			<legend>bananas - 2018-12-18 23:19:47</legend>
+			<p>avoids: cherries<br>
+prefers: apples,donuts,eggplant<br>
+<br></p>
+
+		</fieldset>		<fieldset>
+			<legend>cherries - 2018-12-18 23:19:04</legend>
+			<p>prefers: apples<br>
+<br></p>
+
+		</fieldset>		<fieldset>
+			<legend>donuts - 2018-12-22 01:47:37</legend>
+			<p>avoids: apples<br>
+clean_after_self: no<br>
+<br></p>
+
+		</fieldset>		<fieldset>
+			<legend>eggplant - 2018-12-15 12:33:15</legend>
+			<p><br>There is so much to comment on around here!</p>
+
+		</fieldset><h2 id="confirm_checks">Confirm results check</h2>
+<div class="confirm_results">echo "-----------";
+echo 'bananas' avoid workers 'cherries'
+grep 'bananas' schedule.txt | grep 'cherries'
+echo "-----------";
+echo 'bananas' prefers 'apples'
+grep 'bananas' schedule.txt | grep 'apples'
+echo "-----------";
+echo 'bananas' prefers 'donuts'
+grep 'bananas' schedule.txt | grep 'donuts'
+echo "-----------";
+echo 'bananas' prefers 'eggplant'
+grep 'bananas' schedule.txt | grep 'eggplant'
+echo "-----------";
+echo 'cherries' prefers 'apples'
+grep 'cherries' schedule.txt | grep 'apples'
+echo "-----------";
+echo 'donuts' avoid workers 'apples'
+grep 'donuts' schedule.txt | grep 'apples'
+echo "-----------";
+echo 'donuts' clean after self: 'no'
+grep 'donuts.*donuts' schedule.txt</div>
+EOHTML;
+
+		return [
+			[[], $basic_response],
+			[$small_data, $small_output],
+		];
+	}
+
+	/**
 	 * @dataProvider provideEvalDates
 	public function testEvalDates($worker, $dates, $expected) {
 		$this->calendar->disableWebDisplay();
