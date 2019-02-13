ab7961b7558ed3088a54a9f42efe131422dab768
Willie Northway <willn@umich.edu>
2019-02-12 21:24:06 -0500

    remove type attribute from <script> tags

--- public/display/includes/header.php
+++ public/display/includes/header.php
@@ -5,10 +5,10 @@
 	<link rel="stylesheet" href="display/styles/default.css" type="text/css">
 	<link rel="stylesheet" href="select2/select2.min.css" type="text/css">
 
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-	<script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.3/jquery.dataTables.js"></script>
-	<script type="text/javascript" src="select2/select2.full.min.js"></script>
-	<script type="text/javascript">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script src="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.3/jquery.dataTables.js"></script>
+	<script src="select2/select2.full.min.js"></script>
+	<script>
 		$(document).ready(function() {
 			$('#per_worker').dataTable({
 				'bPaginate': false
