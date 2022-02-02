df313bba72da6bc709857f068cf156e536242d26
Willie Northway <willn@umich.edu>
2022-02-01 22:15:05 -0500

    Correct the find statement for running lint, add more echo statements

--- tests/run.sh
+++ tests/run.sh
@@ -1,7 +1,8 @@
-# do a lint check first...
+# lint check
+echo "--- running lint check first..."
 errs=0
 files_checked=0
-for i in `find ../ -name "*.php -or -name "*.inc""`; do
+for i in `find ../ -name "*.php" -or -name "*.inc"`; do
 	lint=`php -l $i | grep 'Errors'`;
 	[[ ! -z $lint ]] && echo "FAIL $i --> php lint issue" && let errs=errs+1
 	let files_checked=files_checked+1
@@ -12,7 +13,9 @@ then
 fi
 echo "PASS all files pass lint $files_checked"
 
+
 # now run phpunit
+echo "--- running unit tests..."
 for i in `ls *Test.php`; do
 	phpunit $i $1 2>&1 > $1.out;
 	if [[ $? -eq 0 ]]; then
