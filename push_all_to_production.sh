a708dee135bf8c0ab8f6e287c67c451dfec3297d
Willie Northway <willn@umich.edu>
2023-01-21 19:07:37 -0500

    Follow copy of file with a permissions fix

--- push_all_to_production.sh
+++ push_all_to_production.sh
@@ -1,3 +1,6 @@
 
 rsync -e 'ssh -p 1022' -avz public/ gocoho@gocoho.org:/home/gocoho/public_html/meals_scheduling/
 
+#reset permissions
+ssh -i ~/.ssh/id_dsa -p 1022 gocoho@gocoho.org 'chmod -R g-w ~/public_html/meals_scheduling/'
+
