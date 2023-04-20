1cf714fdad1a880abe80aed101918961831c4c79
Willie Northway <willn@umich.edu>
2023-04-19 22:21:11 -0400

    Add a unit test to confirm that debugging has been turned off

---
+++ tests/ConstantsTest.php
@@ -0,0 +1,21 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+global $relative_dir;
+$relative_dir = '../public/';
+#require_once '../public/utils.php';
+require_once '../public/constants.php';
+
+/**
+ * This is simple example to ensure the testing framework functions properly.
+ */
+class ConstantsTest extends TestCase {
+
+	/**
+	 * Test that DEBUG is turned off
+	 */
+	public function test_debug_off() {
+		$this->assertEquals(FALSE, DEBUG);
+	}
+}
+?>
