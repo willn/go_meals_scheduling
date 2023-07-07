256364aed2133775fa0a7a14c6c27d93866ea6f4
Willie Northway <willn@umich.edu>
2023-07-06 23:22:04 -0400

    Update datatables

--- public/display/includes/header.php
+++ public/display/includes/header.php
@@ -5,7 +5,7 @@
 	<link rel="stylesheet" href="display/styles/default.css" type="text/css">
 
 	<script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
-	<script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+	<script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
 
 	<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 	<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
