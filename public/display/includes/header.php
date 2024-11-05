c3fd76e92daeefffddfc2e4f75e9e3b7b394ec49
Willie Northway <willn@umich.edu>
2024-11-04 20:07:21 -0500

    Remove broken datatables, would need to look for a different library for sorting the per-worker table.

--- public/display/includes/header.php
+++ public/display/includes/header.php
@@ -5,17 +5,9 @@
 	<link rel="stylesheet" href="display/styles/default.css" type="text/css">
 
 	<script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
-	<script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
 
 	<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 	<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-	<script>
-		$(document).ready(function() {
-			$('#per_worker').dataTable({
-				'bPaginate': false
-			});
-		} );
-	</script>
 
 <?php
 if (isset($_REQUEST['worker'])) {
