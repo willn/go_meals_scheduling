926f563d9fe80e29acd8b162a98fdbbc58d0e43f
Willie Northway <willn@umich.edu>
2026-07-16 23:34:37 -0400

    simplify

--- push_all_to_production.sh
+++ push_all_to_production.sh
@@ -2,6 +2,6 @@
 rsync -e 'ssh' -avz --exclude '*.swp' public/ gocoho@gocoho.org:/home/gocoho/public_html/meals_scheduling/
 
 #reset permissions
-ssh -i ~/.ssh/id_dsa gocoho@gocoho.org 'cd ~/public_html/meals_scheduling/ && ~/bin/fix_web_perms.sh';
+ssh gocoho@gocoho.org 'cd ~/public_html/meals_scheduling/ && ~/bin/fix_web_perms.sh';
 
 
